9f633226aae876f1ad289fb618fc86832a3de5e1
Cecilia urrutia <urrutia.cici@gmail.com>
2023-10-06 10:56:09 -0300

    Conecto database

--- djflix/settings.py
+++ djflix/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-#from decouple import config
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-SECRET_KEY = SECRET_KEY = 'django-insecure-41dfcgex&0lwys1^^2tk=d$(8y@9w(8+r1xdyi!(cy28%1^=vk'
+#SECRET_KEY =  'django-insecure-41dfcgex&0lwys1^^2tk=d$(8y@9w(8+r1xdyi!(cy28%1^=vk'
+SECRET_KEY =  config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -76,7 +77,6 @@ WSGI_APPLICATION = 'djflix.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -84,10 +84,10 @@ DATABASES = {
         'PORT': '',
         'NAME': config('DATABASE_NAME'),
         'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
+        'PASSWORD': config('DATABASE_PASSWORD')
     }
 }
-'''
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -125,8 +125,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
+    BASE_DIR / "static"
+    
 ]
 
 
