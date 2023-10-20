606fa715b2235f9dd813fa024781d3506623e782
Cecilia urrutia <urrutia.cici@gmail.com>
2023-10-20 10:14:36 -0300

    corrijo models, comento el campo manytomanyfields

--- administracion/models.py
+++ administracion/models.py
@@ -1,34 +1,48 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
 class Persona(models.Model):
     nombre_apellido = models.CharField(verbose_name='Nombre y Apellido', max_length=500)
     direccion = models.CharField(verbose_name='Direccion', max_length=700)
-    dni = models.TextField(verbose_name= "DNI", max_length=350)
+    dni = models.IntegerField(verbose_name= "DNI")
     email = models.EmailField(verbose_name="Email", max_length=250)
     pais = models.CharField(verbose_name='Pais', max_length=200)
     ciudad = models.CharField(verbose_name='Ciudad', max_length=200)
 
     def __str__(self) -> str:
         return f'{self.nombre_apellido} - {self.dni} - {self.direccion}  -  {self.email} - {self.pais} - {self.ciudad} '
- 
+
+
+    def clean_dni(self):
+        #len(str(self.cleaned_data['dni'])) == 8
+        if (0 < self.cleaned_data['dni'] <= 99999999):
+            raise ValidationError("El dni es un número positivo y no tiene más de 8 dígitos")
+        
+        return self.cleaned_data['dni']
+
+    def clean_email(self):
+        if self.cleaned_data['email'] == 'email':
+            raise ValidationError("El mail que está usando ya está siendo usado")
+        
+        return self.cleaned_data['email']
+
+
     class Meta:
      abstract = True
+     
     
-class Suscripcion(models.Model):
-        tipo_plan = models.CharField(verbose_name="Tipo de Plan", max_length=150)
-        baja = models.DateField(verbose_name="baja")
         
-def __str__(self) -> str:
-        return f'{self.tipo_plan} -{self.baja}'
-          
 class Suscriptor(Persona):
     fecha_inicio = models.DateField(verbose_name="Fecha de Inicio")
-    tipo_plan = models.ForeignKey(Suscripcion, on_delete=models.CASCADE)
-   
+    tipo_plan = models.CharField(verbose_name="plan", max_length=200)
+    baja = models.DateField(verbose_name="baja", default=False)
+
+
 class Categorias(models.Model):
     nombre = models.CharField(verbose_name="Nombre", max_length=100)
+
 
 class Peliculas(models.Model):
     nombre = models.CharField(verbose_name="Nombre Pelicula", max_length=200)
@@ -40,10 +54,11 @@ class Peliculas(models.Model):
     categoria = models.ForeignKey(Categorias, on_delete=models.CASCADE)
     es_serie = False
     enlace = models.TextField(verbose_name="Enlace", max_length=500)
-    suscriptor = models.ManyToManyField(Suscriptor, through="Visualizaciones")
+    #suscriptor = models.ManyToManyField(Suscriptor, through="Visualizaciones")
     baja = models.DateField(verbose_name="baja")
 
-class Capitulos()
+
+class Capitulos(models.Model):
     capitulo_numero= models.IntegerField(verbose_name= "Capitulo Número",null=True)
     nombre_capitulo = models.CharField(verbose_name="Nombre del Capitulo", max_length=200)
     id_pelicula = models.ForeignKey(Peliculas, on_delete=models.CASCADE)
@@ -52,6 +67,7 @@ class Capitulos()
     duracion = models.DateTimeField(verbose_name="Duracion")
     enlace = models.TextField(verbose_name="Enlace", max_length=500)
     baja = models.DateField(verbose_name="baja")
+
 
 class Visualizaciones(models.Model):
     id_suscriptor = models.ForeignKey(Suscriptor, on_delete=models.CASCADE)
