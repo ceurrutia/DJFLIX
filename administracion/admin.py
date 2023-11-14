6289ec48168ceb3d4d37b22ed3c0171608b49b73
InesQuinteros <115841025+InesQuinteros@users.noreply.github.com>
2023-11-13 21:50:53 -0300

    En admin de Django, que SusXPeli se CRUD en pelic

--- administracion/admin.py
+++ administracion/admin.py
@@ -1,12 +1,30 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.fields.related import ManyToManyField
+from django.forms.models import ModelMultipleChoiceField
+from django.http.request import HttpRequest
 from administracion.models import Pelicula, Serie, Suscriptor, Categorias, Visualizaciones_pelicula
 
 
 # Register your models here.
 
-admin.site.register(Pelicula)
+#admin.site.register(Pelicula)
 admin.site.register(Serie)
 admin.site.register(Visualizaciones_pelicula)
-admin.site.register(Suscriptor)
+#admin.site.register(Suscriptor)
 admin.site.register(Categorias)
 
+# admin.py
+#Pelicula
+#    Suscriptor
+#Libro
+#    autor
+class SuscriptorInline(admin.TabularInline):
+    model = Pelicula.suscriptor.through  # Modelo de la relación muchos a muchos
+
+class PeliculaAdmin(admin.ModelAdmin):
+    inlines = [SuscriptorInline,]
+#    filter_horizontal = ('suscriptores')
+
+admin.site.register(Suscriptor)
+admin.site.register(Pelicula, PeliculaAdmin)
